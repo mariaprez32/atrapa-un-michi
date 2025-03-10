0db344f81d5a10dc56d241fa9454016d01c1c38e
Aelnordev <loreriba99@gmail.com>
2025-03-10 19:10:56 +0100

    add structure to main and app, style app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AdoptPage from "./pages/AdoptPage";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <header>
+        <h1>
+          <Link to="/">Atrapa un michi</Link>
+        </h1>
+      </header>
+      <main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/adopt" element={<AdoptPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      </main>
+      <footer>
+        <p>© 2025 Purrfect Companions. All rights reserved.</p>
+        <p>Contact us: adopta@atrapaunmichi.com</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
