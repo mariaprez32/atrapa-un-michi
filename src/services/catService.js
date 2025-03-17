a2ccb48e6fa7129da1f2b8e2e61d2757e620c1bd
Aelnordev <loreriba99@gmail.com>
2025-03-17 10:11:55 +0100

    delete unnecessary data prop

--- src/services/catService.js
+++ src/services/catService.js
@@ -1,9 +1,9 @@
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=10';
+const API_URL = 'https://api.thecatapi.com/v1/images';
 
 
 export const fetchCats = async() => {
   try {
-    const response = await fetch(`${API_URL}`);
+    const response = await fetch(`${API_URL}/search?limit=10`);
     
     if (!response.ok) {
       console.error(`Error: ${response.status}`);
@@ -13,13 +13,14 @@ export const fetchCats = async() => {
     const data = await response.json();
     console.log("Datos recibidos:", data);
     
+    //crea una copia transformada de un array de objetos gatos (propiedades del objeto cat, 
+    // que está dentro del array data)
     return data.map((cat) => ({
       id: cat.id,
       image: cat.url,
       name: getRandomName(),
       description: getRandomDescription(),
-      tag: getRandomTag(),
-      buttonText: '¡Adóptame!'
+      tag: getRandomTag()
     }));
     
   } catch (error) {
@@ -28,10 +29,9 @@ export const fetchCats = async() => {
   }
 };
 
-/**
- * Obtiene los detalles de un gato específico por su ID
- * @param {string} id - ID del gato
- */
+
+
+
 export const fetchCatById = async (id) => {
   if (!id) {
     console.error('Se requiere un ID para obtener un gato específico');
@@ -40,7 +40,7 @@ export const fetchCatById = async (id) => {
 
   try {
    
-    const response = await fetch(`${API_URL}/images/${id}`);
+    const response = await fetch(`${API_URL}/${id}`);
     
     if (!response.ok) {
       console.error(`Error al obtener el gato: ${response.status}`);
@@ -49,12 +49,12 @@ export const fetchCatById = async (id) => {
     
     const data = await response.json();
     
+    //devuelve el objeto gato
     return {
       id: data.id,
       image: data.url,
       name: getRandomName(),
       description: getRandomDescription(),
-      status: 'Disponible para adopción',
       tag: getRandomTag()
     };
     
